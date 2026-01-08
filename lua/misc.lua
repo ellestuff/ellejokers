7790ab61a8c0615c3aa0b8923b59df4a9191d65b
ellestuff. <ellef1566@gmail.com>
2026-01-08 22:39:34 +0000

    voucher fix
    oops

--- lua/misc.lua
+++ lua/misc.lua
@@ -16,6 +16,7 @@ SMODS.Tag {
 	end
 }
 
+-- Voucher behaviour is in the hooks below
 SMODS.Voucher {
 	key = "mixup",
 	atlas = "vouchers",
@@ -27,18 +28,20 @@ SMODS.Voucher {
 	atlas = "vouchers",
 	pos = {x=1,y=0},
 	cost = 10,
-	requires = "v_elle_mixup"
+	requires = {"v_elle_mixup"}
 }
 
-local rarity1 = {[1]=true,[2]=true,[3]=true}
-local rarity2 = {1,2,3}
+local rarity1 = {[1]=true,[2]=true,[3]=true}	-- Rarities to check for, formatted for easier checking
+local rarity2 = {1,2,3} 						-- Rarities to pick from
 
+-- Make the rarities equally likely
 local pr_hook = SMODS.poll_rarity
 function SMODS.poll_rarity(_pool_key, _rand_key, ...)
 	local pr = pr_hook(_pool_key, _rand_key, ...)
 	return (#SMODS.find_card("v_elle_mixup")>0 and rarity1[pr]) and pseudorandom_element(rarity2, "elle_rarity_mix") or pr
 end
 
+-- 1 in 20 chance for shop Jokers to be Legendary
 local gcp_hook = get_current_pool
 function get_current_pool(_type, _rarity, _legendary, _append)
     if #SMODS.find_card("v_elle_breakthrough")>0 and _type == 'Joker' and _append == 'sho' and pseudorandom('ellerar'..G.GAME.round_resets.ante.._append, 1, 20)==1 then _legendary = true end
