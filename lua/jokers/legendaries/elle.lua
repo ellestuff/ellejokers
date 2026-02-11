646cc41571a20603eb23d972a2c13152220091a2
ellestuff. <ellef1566@gmail.com>
2026-02-11 22:20:09 +0000

    guh

--- lua/jokers/legendaries/elle.lua
+++ lua/jokers/legendaries/elle.lua
@@ -37,3 +37,48 @@ function Card:click()
         check_for_unlock({type = "elle_toggle_palette"})
     end
 end
+
+
+-- this is largely taken from cryptid								and alexi <3
+-- only runs if before the commit that added menu_card
+if V(SMODS.version) < V("1.0.0~BETA-1330a-STEAMODDED") then
+	local gmm = Game.main_menu
+	Game.main_menu = function(change_context, ...)
+		local ret = gmm(change_context, ...)
+
+		G.E_MANAGER:add_event(Event({
+			trigger = "after",
+			delay = 0.05,
+			blockable = false,
+			blocking = false,
+			func = function()
+				local newcard = Card(
+					G.title_top.T.x,
+					G.title_top.T.y,
+					G.CARD_W,
+					G.CARD_H,
+					G.P_CARDS.empty,
+					G.P_CENTERS.j_elle_elle, -- replace this with whatever card
+					{ bypass_discovery_center = true }
+				)
+
+				-- avoid changing the cardarea dimensions if something did that already
+				if #G.title_top.cards <= 1 then
+					G.title_top.T.w = G.title_top.T.w * 1.7675
+					G.title_top.T.x = G.title_top.T.x - 0.8
+				end
+
+				G.title_top:emplace(newcard)
+				newcard:start_materialize(nil, false, 1)
+				newcard.T.w = newcard.T.w * 1.1 * 1.2
+				newcard.T.h = newcard.T.h * 1.1 * 1.2
+				newcard.no_ui = true
+				newcard:set_sprites(newcard.config.center)
+
+				return true
+			end
+		}))
+
+		return ret
+	end
+end
