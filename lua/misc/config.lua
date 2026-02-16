936f8730f003a17410131320593ba7beb58f3d47
ellestuff. <ellef1566@gmail.com>
2026-02-16 01:04:11 +0000

    wip test thing

--- lua/misc/config.lua
+++ lua/misc/config.lua
@@ -64,6 +64,9 @@ local function toggle_palette(args)
 	check_for_unlock({type = "elle_toggle_palette"})
 end
 
+-- ...you ever feel self conscious about your art being too "weird"?
+-- yeahhh that's why i added this toggle.
+
 local puritan_cards = {
 	'j_elle_vivian',
 	'j_elle_feri'
@@ -74,12 +77,58 @@ function ellejokers.puritan_sprite_update()
 	end
 end
 
-local function toggle_streamer(args)
-	ellejokers.puritan_sprite_update()
-	if not ellejokers.mod_data.config.puritan then check_for_unlock({type = "elle_puritan"}) end
+
+
+local function show_element(container)
+    if container and not container.config.original_object_visible then
+        container.config.original_object_visible = true
+
+        container.config.object:remove()
+        container.config.object = container.config.original_object
+        container.config.original_object.states.visible = true
+        container.config.object.config.parent = container;
+
+        (container.UIBox or container):recalculate()
+    end
+end
+
+local function hide_element(container)
+	if container and container.config.original_object_visible then
+		container.config.original_object_visible = false
+		container.config.original_object.states.visible = false
+		container.config.object = Moveable();
+
+		(container.UIBox or container):recalculate()
+	end
 end
 
 SMODS.current_mod.config_tab = function()
+	local t = UIBox{definition={n=G.UIT.ROOT, config={colour=G.C.CLEAR}, nodes = {
+		{n=G.UIT.R, config={align = "cm"}, nodes={
+			{n=G.UIT.T, config={text = '"gooner mod gooner mod" stfu', scale = 0.30, colour = G.C.WHITE}}
+		}}
+	}},config={}}
+	t.states.visible = false
+
+	local nodething = UIBox{definition={n=G.UIT.ROOT, config={colour=G.C.CLEAR}, nodes = {
+		{ n = G.UIT.O,
+			config = {
+				original_object_visible = false,
+				original_object = t,
+				object = Moveable(),
+				id = "textthing"
+		}}
+	}},config={}}
+
+
+	local function toggle_streamer(args)
+		ellejokers.puritan_sprite_update()
+		if not ellejokers.mod_data.config.puritan then check_for_unlock({type = "elle_puritan"}) end
+		
+		local n = nodething:get_UIE_by_ID("textthing")
+		if(ellejokers.mod_data.config.puritan) then hide_element(n) else show_element(n) end
+	end
+
 	return {n = G.UIT.ROOT, config = {
 		emboss = 0.05,
 		r = 0.1,
@@ -124,13 +173,20 @@ SMODS.current_mod.config_tab = function()
 				padding = 0.1,
 				colour = G.C.L_BLACK
 			}, nodes = {
-			{n = G.UIT.C, config = {align = "cl", padding = 0.1}, nodes = {
+			{n = G.UIT.C, config = {align = "cm", padding = 0.1}, nodes = {
 				toggle_with_desc({callback  = toggle_streamer,
 					label = "Streamer Mode",
 					ref_table = ellejokers.mod_data.config,
 					ref_value = 'puritan',
 					desc = { text = {"Changes some card","sprites that may be","considered suggestive"} }
-				})
+				}),
+				{n=G.UIT.R, config={align = "cm"}, nodes={
+					{n=G.UIT.T, config={text = "Do not disable if streaming, trust me.", scale = 0.30, colour = G.C.WHITE}}
+				}},
+				{n=G.UIT.O, config={
+					align = "cm",
+					object = nodething
+				}, nodes={}},
 			}},
 			{n = G.UIT.C, nodes = {{n = G.UIT.O, config = { object = create_display_card(pseudorandom_element(puritan_cards,"elle_puritan_preview")) }}}}
 			
@@ -142,10 +198,10 @@ function G.FUNCS.conf_elle_palette(args)
 	ellejokers.mod_data.config.pixel_shader.palette = args.cycle_config.current_option
 end
 
-
-
 function G.FUNCS.conf_elle_palette_credit(args)
 	args.config.ref_table = ellejokers.palettes[ellejokers.mod_data.config.pixel_shader.palette]
 end
 
 
+
+--
