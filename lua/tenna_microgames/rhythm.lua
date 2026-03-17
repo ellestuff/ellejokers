d5f20f308d3e432ad4a816d1d76a8c783ec1dbdc
ellestuff. <ellef1566@gmail.com>
2026-03-17 23:46:06 +0000

    why are the notes not synced up what

--- lua/tenna_microgames/rhythm.lua
+++ lua/tenna_microgames/rhythm.lua
@@ -50,8 +50,9 @@ local function load_chartdata(pos,length)
 			-- Account for wraparound in song looping
 			local rel_time = (v[1]+song_len-pos)%song_len
 			if rel_time>=0 and rel_time<length then
-				v[1] = rel_time
-				chart_notes[track][#chart_notes[track]+1] = v
+				local note = SMODS.shallow_copy(v) -- Don't use the same references from the actual chart when overriding position
+				note[1] = rel_time
+				chart_notes[track][#chart_notes[track]+1] = note
 			end
 		end
 		table.sort(chart_notes[track],function(a,b)return a[1]<b[1] end)
@@ -68,7 +69,7 @@ local microgame = {
 		string.upper(binds.left[1]),
 		string.upper(binds.right[1])
 	} end,
-	duration = 5
+	duration = 10
 }
 
 local combo = 0
@@ -82,12 +83,13 @@ for k, v in pairs(sprites) do
 end
 
 microgame.init = function()
+	print("INIT!!!")
 	init_inputs(binds)
 	local stream = get_song_source().sound
 	song_len = stream:getDuration()
 	start_pos = stream:tell()
-	song_pos = start_pos
-	load_chartdata(song_len-5,10)
+	song_pos = 0
+	load_chartdata(start_pos,10)
 end
 
 microgame.update = function(dt)
@@ -96,15 +98,42 @@ microgame.update = function(dt)
 		play_sound("elle_utdr_hurt")
 		ellejokers.microgame.hits = ellejokers.microgame.hits+1
 	end
-	song_pos = song_pos + dt
+	song_pos = song_pos+dt
 end
 
+-- bpm = 148
+
 microgame.draw = function()
+	local f = love.graphics.getFont()
+	love.graphics.setFont(ellejokers.undertale_font)
 	love.graphics.clear(0,0,0.5,1)
 	love.graphics.setColor(1,1,1)
 	love.graphics.draw(sprites.bg,0,0,0,1,1)
-	love.graphics.print(scrollspeed,ellejokers.undertale_font,100,100)
-	love.graphics.print(combo,ellejokers.undertale_font,100,200)
+	love.graphics.print(song_pos,100,100)
+	love.graphics.print(microgame.duration-ellejokers.microgame.timer,100,120)
+
+	local y = 200
+	for k, v in pairs(chart_full) do
+		love.graphics.print(k,10,y-30)
+		for _, v2 in ipairs(v) do
+			local pos = 10+v2[1]*20
+			love.graphics.rectangle("fill",pos,y+20*v2[2],1,10)
+		end
+		y = y+100
+	end
+	love.graphics.rectangle("fill",10,200,1,240)
+	love.graphics.rectangle("fill",10+(song_pos+start_pos)*20,200,1,240)
+
+	--[[love.graphics.rectangle("fill",200,400,180,2)
+	for i, v in ipairs(chart_notes.lead) do
+		local relt = v[1]-song_pos
+		local pos = 400-relt*200*scrollspeed
+		if pos>20 then
+			love.graphics.rectangle("fill",200+100*v[2],pos,80,20)
+		end
+	end]]
+
+	love.graphics.setFont(f)
 end
 
 return microgame
