79f67be9a62b74dbcba672ec6ff879f245c887cf
ellestuff. <ellef1566@gmail.com>
2026-05-14 15:00:47 +0100

    chloe retrigger fix

--- lua/residents/chloe.lua
+++ lua/residents/chloe.lua
@@ -14,7 +14,7 @@ ellejokers.Resident {
 		loc_vars = function(self, card) return { card.ability.extra.req, card.ability.extra.count } end
 	},
 	calculate = function(self, card, context)
-		if context.discard and not context.other_card.debuff and not context.blueprint then
+		if context.discard and not context.other_card.debuff and not context.retrigger_joker then
 			card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chip_mod
 			
 			local face = context.other_card:is_face()
